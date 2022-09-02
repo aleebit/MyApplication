bdce55273b1530fc90203a05ccf1d88a4fc75da3
Ayumi Lee <alee@madisoncollege.edu>
2022-09-01 22:17:59 -0500

    API Client

---
+++ app/src/main/java/com/example/myapplication/APIClient.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+object ApiClient {
+    var BASE_URL = "https://fetch-hiring.s3.amazonaws.com/"
+    private var retrofit: Retrofit? = null
+    fun getClient(): Retrofit? {
+        if (retrofit == null) {
+            retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+        return retrofit
+    }
+}
